6c5b879f857448235f4db0ccb8c6491a96e85def
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-03-28 00:06:58 -0300

    homepage

---
+++ lib/Jogar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+  void _exibirResultado() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("imagens/logo.png"),
+            GestureDetector(
+                onTap: _exibirResultado,
+                child: Image.asset("imagens/botao_jogar.png"))
+          ],
+        ),
+      ),
+    );
+  }
+}
